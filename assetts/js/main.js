b12b2fc52adb9a77547bd64d0d793b7baae5bf66
JanLascala <lascala.jan@gmail.com>
2025-03-03 15:20:23 +0100

    no progress

--- assetts/js/main.js
+++ assetts/js/main.js
@@ -42,13 +42,20 @@ fetch(cardsEndpoint)
         //overlay function
         const allphotos = document.querySelectorAll('.card-img-top');
 
+
+
         allphotos.forEach(img => {
             img.addEventListener('click', function () {
                 overLayEl.classList.add('d-block');
                 overLayEl.classList.remove(`d-none`)
                 //add imgs 
+                for (let i = 0; i < allphotos.length; i++) {
+                    const singlephoto = allphotos[i];
+                    return singlephoto
+                }
 
-
+                photoEl.innerText = singlephoto
+                photoEl.src = singlephoto.url
 
 
 
