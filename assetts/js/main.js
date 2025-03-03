1be9fdf605583699028dcc0602e79e8551887554
JanLascala <lascala.jan@gmail.com>
2025-03-03 10:04:20 +0100

    add js fetch

--- assetts/js/main.js
+++ assetts/js/main.js
@@ -1,1 +1,18 @@
 console.log("works fine")
+
+const cardsRow = document.querySelector(`.container, .row`)
+const cardsEndpoint = `https://lanciweb.github.io/demo/api/pictures/`
+
+
+
+fetch(cardsEndpoint)
+    .then(response => response.json())
+    .then(cardData => {
+        console.log(cardData) //works
+    })
+    .catch(Error => {
+
+        console.error(error);
+
+
+    })
