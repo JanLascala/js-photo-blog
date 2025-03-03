afe0a8cc2bd3a73401f99c9f36470e3e836f0073
JanLascala <lascala.jan@gmail.com>
2025-03-03 10:54:53 +0100

    Fixed issues with JS

--- assetts/js/main.js
+++ assetts/js/main.js
@@ -1,6 +1,6 @@
 console.log("works fine")
 
-const cardsRow = document.querySelector(`.container, .row`)
+const cardsRow = document.querySelector(`.container .row`)
 const cardsEndpoint = `https://lanciweb.github.io/demo/api/pictures/`
 
 
@@ -9,6 +9,34 @@ fetch(cardsEndpoint)
     .then(response => response.json())
     .then(cardData => {
         console.log(cardData) //works
+
+        // insert data inside the cards
+
+        cardData.forEach(singleCardData => {
+
+
+            // data of EACH card 
+            console.log(singleCardData);
+
+            const CardMarkup =
+
+                `<div class="col-12 col-md-6 col-xl-4">
+            <div Id="Polaroid" class="card p-3">
+                <div id="Img_container">
+                    <img class="card-img-top" src="${singleCardData.url}" />
+                    <img id="RedPin" width="30" src="./assetts/img/pin.svg" alt="red pin">
+                </div>
+                <div class="card-body text-start">
+                    <p class="photo_date text-body-secondary">${singleCardData.date}</p>
+                    <h3 id="Photo_title" class="card-title">${singleCardData.title}</h3>
+                </div>
+            </div>
+        </div>`
+            cardsRow.insertAdjacentHTML(`beforeend`, CardMarkup)
+        });
+
+
+
     })
     .catch(Error => {
 
@@ -16,3 +44,6 @@ fetch(cardsEndpoint)
 
 
     })
+
+
+
